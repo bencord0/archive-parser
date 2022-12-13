eacea6e5dd7bcbaaedc0b398ae87460c141d111a
Ben Cordero <bencord0@condi.me>
2022-12-13 02:30:01 +0000

    Parse a tweet from the archive

--- src/bin/parse_tweet.rs
+++ src/bin/parse_tweet.rs
@@ -1,11 +1,23 @@
 use archive_parser::TwitterArchive;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version)]
+struct Config {
+    #[arg(long)]
+    archive: PathBuf,
+    #[arg(long)]
+    tweet: usize,
+}
 
 fn main() {
-    let tweet_id = ...;
+    let config = Config::parse();
 
-    let archive = TwitterArchive::from_dir(...)
+    let archive = TwitterArchive::from_dir(config.archive)
         .expect("Load Twitter Archive Directory");
-    let tweet = archive.get_tweet(tweet_id)
+
+    let tweet = archive.get_tweet(config.tweet)
         .expect("Get Tweet");
 
     println!("{tweet}");
